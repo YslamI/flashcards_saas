1013272a0a92859ce5e3dad9cdcdeee7ee143331
YslamI <fillipsenterprise@gmail.com>
2024-08-14 22:04:25 -0400

    Progress until 30th minute of video

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="description" content="Create flashcards to easy and effective study from your text" />
       </Head>
 
-      <AppBar>
+      <AppBar position="static" sx={{width: "100%"}}>
         <Toolbar color="cyan">
           <Typography variant="h6" width="100%" style={{flexGrow: 1}}>Flashcard SaaS</Typography>
           <SignedOut>
@@ -24,6 +24,48 @@ export default function Home() {
           </SignedIn>
         </Toolbar>
       </AppBar>
+
+      <Box 
+      sx={{textAlign: "center"}}>
+        <Typography variant="h2">Welcome to Flashcard</Typography>
+        <Typography variant="h5">
+          {' '}
+          Create flashcards in just one click
+        </Typography>
+        <Button variant="contained" color="primary" sx={{mt: 2}}>Get Started</Button>
+      </Box>
+      <Box sx={{my: 6}}>
+        <Typography variant="h4" components="h2">
+          Features
+        </Typography>
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Magic Flashcards</Typography>
+            <Typography>
+              {' '}
+              Input your text and see the magic of our app! Study faster and effectively from our flashcards!
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {' '}
+              Our intelligent AI turns your text into keywords, perfect for studying 
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Flashcards Everywhere</Typography>
+            <Typography>
+              {' '}
+              Access your flashcards anywhere, anytime, on any device!
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   )
 }
